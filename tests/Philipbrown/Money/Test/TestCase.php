2c720eadd8298b2cd77f0d5809ae783c3a18992e
Philip Brown <phil@ipbrown.com>
2013-11-02 18:42:58 +0000

    Set up tests structure

---
+++ tests/Philipbrown/Money/Test/TestCase.php
@@ -0,0 +1,13 @@
+<?php namespace Philipbrown\Money\Test;
+
+use PHPUnit_Framework_TestCase;
+use Philipbrown\Money\Money;
+
+class TestCase extends PHPUnit_Framework_TestCase {
+
+  public function setUp()
+  {
+    $this->m = new Money;
+  }
+
+}
